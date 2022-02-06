fabf21df7e583d4cd47efe7b920abb45aafd2af6
Roberto Ojeda <ojeda.robertodaniel@gmail.com>
2022-02-05 22:31:01 -0300

    version 0.6 correcciones optimzación

--- src/Components/CartWidget.js
+++ src/Components/CartWidget.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Cart4 } from 'react-bootstrap-icons';
 
 export const CartWidget = () => {
--- src/Components/ItemListContainer.js
+++ src/Components/ItemListContainer.js
@@ -1,6 +1,5 @@
-import React from 'react';
 
-export const ItemListContainer = ({title}) => {
+export const ItemListContainer = ({ title }) => {
     return <h1>{title}</h1>
 };
 
